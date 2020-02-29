96e480003756db07b57eb47b24ccbf92df5894a1
weisman94 <58262005+weisman94@users.noreply.github.com>
2020-02-28 19:30:08 -0500

    Se codifica Array indexado y Asociativos
    Se realizan los Array indexado y Asociativos, en el index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo("Clase 4 bucles,Arreglos y funciones");
+
+// array indexado : son los que php asigna el nombre o identificador
+$arreglo=array("juan","pedro","jose","carolina","alejandra");
+echo("<br>");
+print_r($arreglo);
+echo("<br>..................");
+
+
+//array asociativo: son los que el programador asigna el nombre o identificador
+$usuarios=array('user1'=>"juan",
+                'user2'=>"pedro",
+                'user3'=>"jose",
+                'user4'=>"carolina",
+                'user5'=>"alejandra");
+echo("<br>");
+print_r($usuarios);
+echo("<br>..................");
+
+
+?>
